e7d6981e6211490c4c0726bd91641fb53cf83ff0
Rafael Almeida <rafaelpbcp@gmail.com>
2023-02-05 12:23:47 -0500

    added .env vars, updated requirements, removed pycache

--- capstone_server/settings.py
+++ capstone_server/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=b+#^b!^0uc0os5f2+2e*9^pksh^r-skq2acbj*yucg6@@ymbq'
+import os
+from dotenv import load_dotenv
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
