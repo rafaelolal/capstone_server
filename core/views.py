b05f539ac7b37a65acd60206361e41514e86eead
Rafael Almeida <rafaelpbcp@gmail.com>
2023-02-05 21:16:31 -0500

    idk

--- core/views.py
+++ core/views.py
@@ -1,26 +1,32 @@
-from django.shortcuts import render
-
+from rest_framework.generics import RetrieveAPIView, ListAPIView, CreateAPIView
+from .serializers import QuestionListSerializer, QuestionSerializer, UnitSerializer, AnswerSerializer
 from .models import Unit, Question, Answer
-from rest_framework import viewsets
-from .serializers import QuestionSerializer, UnitSerializer, AnswerSerializer
 
-class QuestionViewSet(viewsets.ModelViewSet):
+class QuestionListView(ListAPIView):
     """
-    API endpoint that allows Units to be viewed or edited.
+    API endpoint that allows Questions to be retrieved.
+    """
+    queryset = Question.objects.all()
+    serializer_class = QuestionListSerializer
+
+class QuestionRetrieveView(RetrieveAPIView):
+    """
+    API endpoint that allows a Question to be retrieved.
     """
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-class UnitViewSet(viewsets.ModelViewSet):
+class UnitRetrieveView(RetrieveAPIView):
     """
-    API endpoint that allows Units to be viewed or edited.
+    API endpoint that allows a Unit to be retrieved.
     """
     queryset = Unit.objects.all()
     serializer_class = UnitSerializer
 
-class AnswerViewSet(viewsets.ModelViewSet):
+# TODO: add protection/permissions
+class AnswerCreateView(CreateAPIView):
     """
-    API endpoint that allows Answers to be viewed or edited.
+    API endpoint that allows an Answer to be created.
     """
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
