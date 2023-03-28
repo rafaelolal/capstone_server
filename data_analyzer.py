fa96dae2a6a460271d345db0a2cbc44fe8bbcb7c
Rafael Almeida <rafaelpbcp@gmail.com>
2023-03-28 13:52:29 -0400

    changes to data analyzer

--- data_analyzer.py
+++ data_analyzer.py
@@ -15,14 +15,22 @@ class MyFunctions:
     def __init__(self):
         self.output = []
 
-    def my_print(self, line):
+    def my_prnt(self, line):
         self.output.append(str(line).strip("\n "))
 
     def my_input(self, *args, **kwargs):
         return input()
 
+    def my_int(self, x):
+        return int(x)
+
     def get_output(self):
         return "\n".join(self.output).strip("\n ")
+
+excluded = list(map(lambda x: x.strip('\r\n '), open('exclude.txt', 'r').readlines()))
+
+pretest_answers = [answer for answer in Answer.objects.all().filter(question=1) if str(answer) not in excluded]
+posttest_answers = [answer for answer in Answer.objects.all().filter(question=6) if str(answer) not in excluded]
 
 pretest_inp = """5 4 2
 XXX.
@@ -42,42 +50,60 @@ XX....XX
 XXXXXX..
 XXXXXX.."""
 
-# requirements
+posttest_inp = """5 3
+1
+6
+4
+3
+1"""
+
+posttest_output = "4"
+
+# pretest requirements
 # exactly two input functions
-# at least two for loops
-# at most one print statement that prints a variable
-# at least one + operator
+# exactly one print statement function
+# at least one for loop
 # at least one int function
 # error-free
 # outputs the correct answer
+# at least one + operator
+
+# posttest requirements
+# exactly two input functions
+# exactly one print function
+# at least one for loop
+# at least one int function
+# error free
+# correct answer
+# at least one comparison operator
+
 def get_pretest_data():
     requirements = 7
     pretest_data = {}
-    for answer in Answer.objects.all().filter(question=1):
-        if answer.unit.key in ['5243', '1041', "9999"]:
+    for answer in pretest_answers:
+        if answer.unit.key in ['5243', '1041', '9999']:
             continue
 
+        code = format_code(answer.content)
         score = 0
 
-        if answer.content.count("input(") == 2:
+        if code.count("my_input(") == 2:
             score += 100/requirements
 
-        if answer.content.count("for ") >= 2:
+        if code.count("for ") >= 1:
             score += 100/requirements
 
-        if answer.content.count("print(") == 1:
-            i = answer.content.find("print(")
-            if answer.content[i+6].isalpha():
-                score += 100/requirements
-
-        if answer.content.count("+") >= 1:
+        if code.count("my_prnt(") == 1:
             score += 100/requirements
 
-        if answer.content.count("int(") >= 1 or answer.content.count("int,") >= 1:
+        if code.count("+") >= 1:
+            score += 100/requirements
+
+        if code.count("my_int(") >= 1 or code.count("my_int,") >= 1:
             score += 100/requirements
 
         try:
-            output = execute_code(answer.content, pretest_inp)
+            output = execute_code(code, pretest_inp)
             score += 100/requirements
 
             if output == pretest_answer:
@@ -91,47 +117,33 @@ def get_pretest_data():
 
     return pretest_data
 
-posttest_inp = """5 3
-1
-6
-4
-3
-1"""
-posttest_output = "4"
-
-# requirements
-# exactly two input functions
-# exactly one print function
-# at least one int function
-# at least one for loop
-# at least one comparison operator
-# error free
-# correct answer
 def get_posttest_data():
     requirements = 7
     posttest_data = {}
-    for answer in Answer.objects.all().filter(question=6):
-        if answer.unit.key in ["9999"]:
+    for answer in posttest_answers:
+        if answer.unit.key in ['9999']:
             continue
 
+        code = format_code(answer.content)
+
         score = 0
-        if answer.content.count("input(") == 2:
+        if code.count("my_input(") == 2:
             score += 100/requirements
 
-        if answer.content.count("int(") >= 2 or answer.content.count("int,") >= 2:
+        if code.count("my_int(") >= 2 or code.count("my_int,") >= 2:
             score += 100/requirements
 
-        if answer.content.count("for ") >= 1:
+        if code.count("for ") >= 1:
             score += 100/requirements
 
-        if any([comparator in answer.content for comparator in ["<", ">", "<=", ">="]]):
+        if any([comparator in code for comparator in ["<", ">", "<=", ">="]]):
             score += 100/requirements
 
-        if answer.content.count("print(") == 1:
+        if code.count("my_prnt(") == 1:
             score += 100/requirements
 
         try:
-            output = execute_code(answer.content, posttest_inp)
+            output = execute_code(code, posttest_inp)
             score += 100/requirements
 
             if output == posttest_output:
@@ -145,22 +157,12 @@ def get_posttest_data():
 
     return posttest_data
 
-def execute_code(code, inp):
-    my_functions = MyFunctions()
-    code = format_code(code)
-    
-    with open("test_file.txt", "w") as file:
-        file.write(inp)
-    
-    with open("test_file.txt", "r") as file:
-        sys.stdin = file
-        exec(code)
-    
-    return my_functions.get_output()
-
 def format_code(code):
-    code = code.replace("print(", "my_functions.my_print(")
+    code = code.replace("print(", "my_functions.my_prnt(")
     code = code.replace("input(", "my_functions.my_input(")
+    code = code.replace("int(", "my_functions.my_int(")
+    code = code.replace("int,", "my_functions.my_int,")
+
     lines = code.split("\n")
     new_lines = []
     for line in lines:
@@ -170,6 +172,18 @@ def format_code(code):
         new_lines.append(new_line)
     code = "\n".join(new_lines)
     return code
+
+def execute_code(code, inp):
+    my_functions = MyFunctions()
+    
+    with open("test_file.txt", "w") as file:
+        file.write(inp)
+    
+    with open("test_file.txt", "r") as file:
+        sys.stdin = file
+        exec(code)
+    
+    return my_functions.get_output()
 
 pre_data = get_pretest_data()
 post_data = get_posttest_data()
